269318f9c49af12405c56428160a302c59e310fa
Mohammadreza Fekri <fekri.mohammadreza86@gmail.com>
2022-01-03 11:57:55 +0330

    it has a small problem. but I will fix

---
+++ app/src/main/java/com/app/colortype/SplashScreenActivity.java
@@ -0,0 +1,29 @@
+package com.app.colortype;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ProgressBar;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    private ProgressBar progressBar;
+
+    private Handler handler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        progressBar = findViewById(R.id.progress);
+        handler = new Handler();
+        handler.postDelayed(() -> {
+            Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }, 3000);
+    }
+}
